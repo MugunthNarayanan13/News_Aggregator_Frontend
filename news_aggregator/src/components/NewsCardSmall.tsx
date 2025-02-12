f96d1632d4059a3453281bbd55ceaeea1af61aea
Thanus Kumaar <thanuskumaara@gmail.com>
2025-02-12 16:26:58 +0530

    Added text truncation to cards

--- news_aggregator/src/components/NewsCardSmall.tsx
+++ news_aggregator/src/components/NewsCardSmall.tsx
@@ -1,11 +1,15 @@
 interface NewsCardSmallProps {
-  title: String;
-  desc: String;
-  pubDate: String;
-  pubName: String;
-  pubLogo: String;
-  sentiment: String;
+  title: string;
+  desc: string;
+  pubDate: string;
+  pubName: string;
+  pubLogo: string;
+  sentiment: string;
 }
+
+const truncateText = (text: string, maxLength: number) => {
+  return text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
+};
 
 export default function NewsCardSmall({
   title,
@@ -19,7 +23,7 @@ export default function NewsCardSmall({
     <>
       <div className="flex flex-col font-roboto sm:w-[300px] lg:w-[400px] sm:h-[150px] lg:h-[200px] border-2 border-black bg-background_light rounded-t-md mb-0">
         <div className="flex flex-row justify-between px-4 mt-1">
-          <div className="font-light text-xl">{title}</div>
+          <div className="font-light text-xl">{truncateText(title, 60)}</div>
           <div className="mt-2">
             {sentiment == "positive" ? (
               <div className="bg-green-400 rounded-full h-4 w-4"></div>
@@ -30,7 +34,7 @@ export default function NewsCardSmall({
             )}
           </div>
         </div>
-        <div className="px-4 py-2 mt-3 font-normal">{desc}</div>
+        <div className="px-4 py-2 mt-3 font-normal">{truncateText(desc, 100)}</div>
       </div>
       <div className="flex flex-row mt-0 sm:w-[300px] lg:w-[400px] sm:h-[35px] lg:h-12 border-2 border-t-0 border-black bg-secondary rounded-b-md">
         <div className="sm:w-[35px] lg:w-12 scale-75 bg-black rounded-full"></div>
