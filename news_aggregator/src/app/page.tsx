189b99785889c7972da4dcd5e22302b26915621a
Thanus Kumaar <thanuskumaara@gmail.com>
2025-02-12 16:21:30 +0530

    Created component with props and sentiment button

--- news_aggregator/src/app/page.tsx
+++ news_aggregator/src/app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <div className="bg-foreground_light h-full">
       <NewsCardSmall
-        title={"hello"}
+        title={"Trump says he could reach trade deal with China, calls talk with Xi 'friendly"}
         desc={"asdf asdf asdf ssdfasd fasdf dasf asdf asdfa dfafasf ad "}
         pubDate={"10-02-2005"}
         pubLogo={"https://asdf.com.asd"}
-        pubName={"Times Of India asdf asf asdf adf "}
+        pubName={"Times Of India"}
         sentiment={"positive"}
       />
     </div>
--- news_aggregator/src/components/NewsCardSmall.tsx
+++ news_aggregator/src/components/NewsCardSmall.tsx
@@ -18,14 +18,23 @@ export default function NewsCardSmall({
   return (
     <>
       <div className="flex flex-col font-roboto sm:w-[300px] lg:w-[400px] sm:h-[150px] lg:h-[200px] border-2 border-black bg-background_light rounded-t-md mb-0">
-        <div className="flex flex-row justify-between px-4">
-          <div className="font-light text-2xl">{title}</div>
-          <div>{sentiment}</div>
+        <div className="flex flex-row justify-between px-4 mt-1">
+          <div className="font-light text-xl">{title}</div>
+          <div className="mt-2">
+            {sentiment == "positive" ? (
+              <div className="bg-green-400 rounded-full h-4 w-4"></div>
+            ) : sentiment == "negative" ? (
+              <div className="bg-red-400 rounded-full h-4 w-4"></div>
+            ) : (
+              <div className="bg-blue-400 rounded-full h-4 w-4"></div>
+            )}
+          </div>
         </div>
+        <div className="px-4 py-2 mt-3 font-normal">{desc}</div>
       </div>
       <div className="flex flex-row mt-0 sm:w-[300px] lg:w-[400px] sm:h-[35px] lg:h-12 border-2 border-t-0 border-black bg-secondary rounded-b-md">
-        <div className="sm:w-[35px] lg:w-12 scale-90 bg-black rounded-full"></div>
-        <div className="flex-1 text-center text-xl mt-2">{pubName}</div>
+        <div className="sm:w-[35px] lg:w-12 scale-75 bg-black rounded-full"></div>
+        <div className="flex-1 text-center text-lg mt-2 pr-4  ">{pubName}</div>
       </div>
     </>
   );
