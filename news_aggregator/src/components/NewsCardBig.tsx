a5abcdf8c455a20c5fa1a3c93a07e862baa40edb
dhileep1 <dhileepansb@gmail.com>
2025-02-19 22:13:44 +0530

    Modified Style of News Cards

--- news_aggregator/src/components/NewsCardBig.tsx
+++ news_aggregator/src/components/NewsCardBig.tsx
@@ -24,7 +24,7 @@ export default function NewsCardBig({
 }: NewsCardBigProps) {
   return (
     <div>
-      <div className="flex flex-col font-roboto sm:w-[250px] md:w-[280px] lg:w-[320px] sm:h-[280px] md:h-[320px] lg:h-[360px] border-2 border-black bg-background_light rounded-t-md mb-0">
+      <div className="flex flex-col font-roboto sm:w-[250px] md:w-[280px] lg:w-[320px] sm:h-[280px] md:h-[320px] lg:h-[360px] border-2 border-none bg-background_light rounded-t-md mb-0">
         <div className="bg-black sm:h-[160px] md:h-[180px] lg:h-[200px] m-2 rounded-lg"></div>
         <div className="flex flex-row justify-between px-3 mt-1 gap-1">
           <div className="font-light text-sm md:text-base lg:text-lg">
@@ -44,7 +44,7 @@ export default function NewsCardBig({
           {truncateText(desc, 150)}
         </div>
       </div>
-      <div className="flex flex-row border-2 border-t-0 border-black bg-secondary rounded-b-md sm:h-[25px] md:h-[30px] lg:h-[35px]">
+      <div className="flex flex-row border-2 border-t-0 border-none bg-secondary rounded-b-md sm:h-[25px] md:h-[30px] lg:h-[35px]">
         <div className="w-[25px] md:w-[30px] lg:w-[35px] scale-75 bg-black rounded-full"></div>
         <div className="flex-1 text-center text-xs md:text-sm lg:text-base">
           {pubName}
