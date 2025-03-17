e54d2f30ebb9fab609b3501fe03540110929dee0
shar3579 <sharmila091104@gmail.com>
2025-03-17 13:19:46 +0530

    Resolved Conflicts

--- news_aggregator/src/app/profile/page.tsx
+++ news_aggregator/src/app/profile/page.tsx
@@ -14,6 +14,7 @@ import { isValidLanguage } from "@/utils/isoConverterLang";
 import { convertCountryToISO } from "@/utils/isoConverterLoc";
 import SubscriptionsSection from "@/components/SubscriptionsSection";
 import FeedbackSection from "@/components/FeebackSection";
+import SavedArticles from "@/components/SavedArticles";
 
 
 interface UserData {
@@ -225,8 +226,14 @@ export default function UserProfile() {
   {activeTab === "subscriptions" && (
           <SubscriptionsSection user={user} setUser={setUser} />
         )}
+
+        
+        
       </div>
       
+         
+
+
     </div>
   );
 }
--- news_aggregator/src/components/SavedArticles.tsx
+++ news_aggregator/src/components/SavedArticles.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NewsCardBigProps } from "./NewsCardBig";
 
 export interface SavedArticlesProps {
